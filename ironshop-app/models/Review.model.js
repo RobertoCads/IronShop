41d67f2cfc09890be170da3f8b253e3a6eaa7330
Shirley Gianina <67474090+shirley-gianina@users.noreply.github.com>
2022-02-13 13:14:11 +0100

    Doubts Review.model.js

--- ironshop-app/models/Review.model.js
+++ ironshop-app/models/Review.model.js
@@ -3,7 +3,8 @@ const { Schema, model } = require('mongoose')
 const reviewSchema = new Schema(
     {
         commentary: String,
-        review: Boolean,
+        review: Boolean,// a base a que vamos a necesitar un true o un false en este caso?.
+        // tendría sentido meter valoraciones? (estrellas de 1 a 5)
         user: [{
             type: Schema.Types.ObjectId,
             ref: "User"
