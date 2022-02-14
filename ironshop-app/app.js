a44a4764e7161bf1fc2e3ca9c8845fd1e5b32c5d
Shirley Gianina <shirley.cd853f@gmail.com>
2022-02-14 18:11:49 +0100

    added Api Cloudinary

--- ironshop-app/app.js
+++ ironshop-app/app.js
@@ -20,21 +20,21 @@ require("./config")(app);
 
 // default value for title local
 
-app.locals.appTitle = "IronShop"
+app.locals.appTitle = "IronShop";
 //title
 
 // 👇 Start handling routes here
 const index = require("./routes/index");
 app.use("/", index);
 
-const auth = require("./routes/auth.routes")
-app.use("/", auth)
+const auth = require("./routes/auth.routes");
+app.use("/", auth);
 
-const user = require("./routes/user.routes")
-app.use("/", user)
+const user = require("./routes/user.routes");
+app.use("/", user);
 
-const admin = require("./routes/admin.routes")
-app.use("/admin", admin)
+const admin = require("./routes/admin.routes");
+app.use("/admin", admin);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
--- ironshop-app/models/Product.model.js
+++ ironshop-app/models/Product.model.js
@@ -1,18 +1,21 @@
 const { Schema, model } = require("mongoose");
 
-
 const productSchema = new Schema(
-    {
-        name: String,
-        description: String,
-        price: String,
-        image: String
+  {
+    name: String,
+    description: String,
+    price: String,
+    image: String,
+    category: {
+      type: String,
+      enum: ["Botella", "Camiseta", "Sudadera", "Taza", "Pegatina"],
     },
-    {
-        timestamps: true
-    }
-)
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const Product = model("Product", productSchema);
-
+Product.syncIndexes();
 module.exports = Product;
