5fb95ee9f3cd3b43020b2031260169ae59177905
Shirley Gianina <shirley.cd853f@gmail.com>
2022-02-15 11:05:09 +0100

    feat:category

--- ironshop-app/routes/index.js
+++ ironshop-app/routes/index.js
@@ -1,8 +1,18 @@
+const Product = require("../models/Product.model");
+
 const router = require("express").Router();
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
 });
+
+router.get("/productos/cat/:category", (req, res, next) => {
+  const { category } = req.params
+  // console.log(category)
+  Product.find({ category }).then((products) =>
+    res.render("products-list", { category, products })
+  );
+})
 
 module.exports = router;
