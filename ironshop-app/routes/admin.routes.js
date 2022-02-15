fc3b298766040811e092de878ced9c0b72661ae8
Roberto Cadenas <robertocads7@gmail.com>
2022-02-15 18:01:28 +0100

    session changes

--- ironshop-app/routes/admin.routes.js
+++ ironshop-app/routes/admin.routes.js
@@ -1,12 +1,18 @@
 const router = require("express").Router();
 const fileUploader = require("../config/cloudinary.config");
+const bcryptjs = require("bcryptjs")
 const Product = require("./../models/Product.model");
+const User = require("../models/User.model")
 
-router.get("/", (req, res, next) => {
-  res.render("admin/profile");
-});
+const saltRounds = 10
 
-router.get("/productos/crear", (req, res, next) => {
+const { isLoggedIn, checkRole } = require("../middleware/route-guard")
+
+// router.get("/", isLoggedIn, checkRole("ADMIN"), (req, res, next) => {
+//   res.render("admin/profile");
+// });
+
+router.get("/productos/crear", isLoggedIn, checkRole("ADMIN"), (req, res, next) => {
   res.render("admin/create-products");
 });
 
@@ -23,11 +29,77 @@ router.post(
   }
 );
 
-router.get("/productos", (req, res, next) => {
+router.get("/productos", isLoggedIn, checkRole("ADMIN"), (req, res, next) => {
   Product.find().then((products) => res.render("admin/products", { products }));
 });
 
-router.post("/productos/:id/borrar", (req, res, next) => {
+
+// EDITAR PRODUCTOS ///////////////////
+
+router.get("/productos/:id/editar", isLoggedIn, checkRole("ADMIN"), (req, res, next) => {
+  const { id } = req.params
+
+  Product
+      .findById(id)
+      .then(products => {
+        res.render("admin/update-products", products)
+      })
+      .catch(err => next(err))
+})
+
+router.post("/productos/:id/editar", fileUploader.single("imageFile"), isLoggedIn, checkRole("ADMIN"), (req, res ,next) => {
+  const { id } = req.params
+  const { name, category, description, price} = req.body
+  console.log(id, req.body)
+  Product
+    .findByIdAndUpdate(id, { name, category, description, price, image: req.file?.path }, {new: true})
+    .then(prueba => {
+      console.log(prueba)
+      res.redirect("/admin/productos")
+    })
+    .catch(err => next(err))
+})
+
+
+
+// EDITAR NOMBRE DE USUARIO ADMIN ///////////////////
+router.get("/", isLoggedIn, checkRole("ADMIN"), (req, res, next) => {
+  const id = req.session.currentUser._id
+
+  User
+      .findById(id)
+      .then(user => {
+        res.render("admin/profile", user)
+      })
+      .catch(err => next(err))
+})
+
+router.post("/", isLoggedIn, checkRole("ADMIN"), (req, res, next) => {
+  const id = req.session.currentUser._id
+  const {username, passwordHash} = req.body
+  console.log(req.body)
+
+  bcryptjs
+    .genSalt(saltRounds)
+    .then(salt => bcryptjs.hash(passwordHash, salt))
+    .then(hashedPassword => User
+      .findByIdAndUpdate(id, { username, passwordHash: hashedPassword}, { new: true }))
+      .then(user => {
+        console.log(user)
+        res.redirect("/admin")
+      })
+      .catch(err => next(err))
+})
+
+
+
+
+
+
+
+
+// BORRAR PRODUCTOS ///////////////////
+router.post("/productos/:id/borrar", isLoggedIn, checkRole("ADMIN"), (req, res, next) => {
   const { id } = req.params;
   Product.findByIdAndDelete(id)
   .then(() => {
@@ -36,11 +108,13 @@ router.post("/productos/:id/borrar", (req, res, next) => {
   })
 });
 
-router.get("/usuarios", (req, res, next) => {
+
+
+router.get("/usuarios", isLoggedIn, checkRole("ADMIN"), (req, res, next) => {
   res.render("admin/users");
 });
 
-router.get("/comentarios", (req, res, next) => {
+router.get("/comentarios", isLoggedIn, checkRole("ADMIN"), (req, res, next) => {
   res.render("admin/reviews");
 });
 
--- ironshop-app/routes/user.routes.js
+++ ironshop-app/routes/user.routes.js
@@ -1,4 +1,49 @@
 const router = require("express").Router()
+const bcryptjs = require("bcryptjs")
+const { isLoggedIn, checkRole } = require("../middleware/route-guard")
+const { isAdmin, isUser } = require("../utils")
+const Product = require("../models/Product.model")
+const User = require("../models/User.model")
+const { Router } = require("express")
+const saltRounds = 10
+
+
+// EDITAR NOMBRE DE USUARIO ////////////////
+router.get("/perfil", isLoggedIn, checkRole("USER"), (req, res, next) => {
+    const id = req.session.currentUser._id
+
+    User
+        .findById(id)
+        .then(user => {
+            res.render("user/user-profile", user)
+        })
+        .catch(err => next(err))
+})
+
+router.post("/perfil", isLoggedIn, checkRole("USER"), (req, res, next) => {
+    const id = req.session.currentUser._id
+    const { username, passwordHash } = req.body
+    console.log(req.body)
+
+    bcryptjs
+        .genSalt(saltRounds)
+        .then(salt => bcryptjs.hash(passwordHash, salt))
+        .then(hashedPassword =>  User
+            .findByIdAndUpdate(id, {username, passwordHash: hashedPassword}, { new: true }))
+        .then(user => {
+            console.log(user)
+            res.redirect("/perfil")
+        })
+        .catch(err => next(err))
+ })
+
+
+
+
+ // CARRITO ////////////////
+ router.get("/carrito", isLoggedIn, checkRole("USER"), (req, res, next) => {
+     res.render("user/cart")
+ })
 
 
 
