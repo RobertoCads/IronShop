6af8d495593dd6b1ee6962fcd5720c71e773a3fd
Shirley Gianina <shirley.cd853f@gmail.com>
2022-02-16 17:52:02 +0100

    comments

--- ironshop-app/routes/index.js
+++ ironshop-app/routes/index.js
@@ -1,17 +1,17 @@
 const Product = require("../models/Product.model");
+const Rating = require("../models/Rating.model");
 
 const router = require("express").Router();
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   Product.find({ featured: true })
-  .limit(3)
-  .then((products) =>  res.render("index", {  products }))
+    .limit(3)
+    .then((products) => res.render("index", { products }));
 });
 
 router.get("/productos/cat/:category", (req, res, next) => {
   const { category } = req.params;
-  // console.log(category)
   Product.find({ category }).then((products) =>
     res.render("products-list", { category, products })
   );
@@ -20,9 +20,25 @@ router.get("/productos/cat/:category", (req, res, next) => {
 router.get("/producto/:id", (req, res, next) => {
   const { id } = req.params;
 
-  Product.findById(id)
-    .then((product) => {
-      res.render("product-details", { product });
+  Promise.all([
+    Product.findById(id),
+    Rating.find({ product: id })
+  ])
+
+  .then(([product, ratings]) => {
+    res.render("product-details", { product, ratings })
+    console.log(product, ratings)
+  })
+})
+
+router.post("/producto/:id", (req, res, next) => {
+  const { id } = req.params;
+  const { comment, rating } = req.body;
+  const user = req.session.currentUser._id;
+  Rating.create({ comment, user, rating, product: id })
+    .then(() => {
+      const url = `/producto/${id}`;
+      res.redirect(url);
     })
     .catch((err) => next(err));
 });
